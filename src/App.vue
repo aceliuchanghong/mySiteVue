5d95b45b4684655fff713a7bfb567868d6691f65
Lawrence-AIP <liuch@allinfinance.com>
2023-09-21 10:34:21 +0800

    不懂

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <!-- https://developer.mozilla.org/zh-CN/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Vue_getting_started-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App.."/>
+  <TestX msg="QAQ"/>
 </template>
+
+<!--帮我引入 ./components/TestX.vue 到这个文件 -->
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// import TestX from './components/TestX.vue'
+
 
 export default {
   name: 'App',
@@ -13,6 +18,10 @@ export default {
     HelloWorld
   }
 }
+
+
+
+
 </script>
 
 <style>
---
+++ src/components/TestX.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestX',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
